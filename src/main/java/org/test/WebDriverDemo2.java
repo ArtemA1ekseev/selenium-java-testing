7dbf7244f79a76077948e83b0ae16f21df4fddf9
arste <a1ekseevartem.ru@gmail.com>
2024-04-03 12:51:48 +0400

    add class WebDriverDemo2.java. Adding comments.

---
+++ src/main/java/org/test/WebDriverDemo2.java
@@ -0,0 +1,25 @@
+package org.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverDemo2 {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe"); //setting the path for chrome driver
+
+        WebDriver driver = new ChromeDriver(); //creating object for chrome driver
+
+        // 1. По id
+        driver.get("http://www.urn.su/ui/basic_test/"); //opens the browser and navigates to the URL
+
+        WebElement searchField =
+                driver.findElement(By.id("name1"));
+
+        searchField.sendKeys("topbicycle.ru");
+        searchField.submit();
+    }
+}
